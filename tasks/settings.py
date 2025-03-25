1107326ab9848af26c01d418b417fc1c30e6bcfc
MatRos-sf <mateuszrosenkranz@gmail.com>
2025-03-25 18:38:18 +0100

    + update gitignore (add: static/tmp/) + update settings file, create UPLOAD_TEMP_FOLDER variable

--- tasks/settings.py
+++ tasks/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "text_processing",
 ]
 
 MIDDLEWARE = [
@@ -125,3 +126,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Temp folder for uploaded files
+UPLOAD_TEMP_FOLDER = BASE_DIR / "static" / "tmp"
