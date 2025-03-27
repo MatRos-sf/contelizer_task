ca304612f33183749a803a84be094d37a2ebda07
MatRos-sf <mateuszrosenkranz@gmail.com>
2025-03-27 19:04:51 +0100

    update pesel_validator templates

--- pesel_validator/forms.py
+++ pesel_validator/forms.py
@@ -4,7 +4,9 @@ from .pesel import Pesel
 
 
 class PeselForm(forms.Form):
-    pesel = forms.CharField(max_length=11)
+    pesel = forms.CharField(
+        max_length=11, widget=forms.TextInput(attrs={"class": "form-control", "row": 1})
+    )
 
     def clean_pesel(self) -> str:
         pesel = self.cleaned_data.get("pesel")
